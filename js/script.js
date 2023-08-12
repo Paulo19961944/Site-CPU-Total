cfab5be7095aced431c5c1bfe31f226c33bd3691
Paulo <106405308+Paulo19961944@users.noreply.github.com>
2023-08-12 10:21:39 -0300

    Update script.js

--- js/script.js
+++ js/script.js
@@ -3,22 +3,15 @@ let inputCelsius = document.getElementById('input1');
 let inputFahrenheit = document.getElementById('input2');
 let button = document.getElementById('btn')
 
-
-
 // Calcula a Diferença
-
 function Calcular() {
     let celsius = parseFloat(inputCelsius.value);
     let fahrenheit = parseFloat(inputFahrenheit.value);
-
     let calculatedFahrenheit = (celsius * 1.8) + 32;
     let calculatedCelsius = (fahrenheit - 32) / 1.8;
-
     console.log(calculatedCelsius);
     console.log(calculatedFahrenheit);
-
 }
-
 function API(){
     // Fazendo uma requisição HTTP para a API
 fetch('https://api.chucknorris.io/jokes/random')
@@ -26,7 +19,6 @@ fetch('https://api.chucknorris.io/jokes/random')
   .then(data => {
     // Extraindo a piada da resposta da API
     const joke = data.value;
-
     // Exibindo a piada no console ou em algum elemento HTML da sua página
     console.log(joke);
     // ou
@@ -35,12 +27,8 @@ fetch('https://api.chucknorris.io/jokes/random')
   .catch(error => {
     console.log('Ocorreu um erro ao obter a piada:', error);
   });
-
 }
-
-
 while(true){
     Calcular()
-    API()
-    
+    API()   
 }
